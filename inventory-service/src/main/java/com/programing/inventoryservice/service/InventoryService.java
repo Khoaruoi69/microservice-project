642a27576cc4c96ad8e4ce70a2182ea0797a1570
KhoaNguyenDangAnh <khoa.nguyen.d@bstarsolutions.com>
2023-10-16 14:08:04 +0700

    complete Circuit Breakers

--- inventory-service/src/main/java/com/programing/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/programing/inventoryservice/service/InventoryService.java
@@ -3,6 +3,7 @@ package com.programing.inventoryservice.service;
 import com.programing.inventoryservice.dto.InventoryResponse;
 import com.programing.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +18,11 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Wait started");
+        Thread.sleep(10000);
+        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                         InventoryResponse.builder()
--- order-service/src/main/java/com/programing/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programing/orderservice/service/OrderService.java
@@ -25,7 +25,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
 
-    public void placeOrder(OrderRequest orderRequest) {
+    public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -54,6 +54,7 @@ public class OrderService {
 
         if(allProductsInStock){
             orderRepository.save(order);
+            return "Order Placed Successfully";
         } else {
             List<InventoryResponse> result = Arrays.stream(inventoryResponseArray).filter(item -> !order.getOrderLineItemsList().contains(item)).collect(Collectors.toList());
             log.info(result.toString());
