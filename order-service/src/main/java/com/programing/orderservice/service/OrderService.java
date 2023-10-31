f5a42ed57732ba5bfe4d63da8b2d98a9ddb7a859
KhoaNguyenDangAnh <khoa.nguyen.d@bstarsolutions.com>
2023-10-31 15:20:30 +0700

    add kafka NotificationService

--- order-service/src/main/java/com/programing/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programing/orderservice/service/OrderService.java
@@ -1,13 +1,17 @@
 package com.programing.orderservice.service;
 
+import brave.Span;
+import brave.Tracer;
 import com.programing.orderservice.dto.InventoryResponse;
 import com.programing.orderservice.dto.OrderLineItemsDto;
 import com.programing.orderservice.dto.OrderRequest;
+import com.programing.orderservice.event.OrderPlacedEvent;
 import com.programing.orderservice.model.Order;
 import com.programing.orderservice.model.OrderLineItems;
 import com.programing.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -24,6 +28,8 @@ import java.util.stream.Collectors;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -40,26 +46,36 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .toList();
 
+        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
+
         // Call Inventory Service, and place order if product is in
         // stock
-        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory",
-                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block();
+        try (Tracer.SpanInScope isLookup = tracer.withSpanInScope(inventoryServiceLookup.start())) {
 
-        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
-                .allMatch(InventoryResponse::isInStock);
+            inventoryServiceLookup.tag("call", "inventory-service");
+            // Call Inventory Service, and place order if product is in
+            // stock
+            InventoryResponse[] inventoryResponsArray = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
 
-        if(allProductsInStock){
-            orderRepository.save(order);
-            return "Order Placed Successfully";
-        } else {
-            List<InventoryResponse> result = Arrays.stream(inventoryResponseArray).filter(item -> !order.getOrderLineItemsList().contains(item)).collect(Collectors.toList());
-            log.info(result.toString());
-            throw new IllegalArgumentException("Product is not in stock, please try again later");
+            boolean allProductsInStock = Arrays.stream(inventoryResponsArray)
+                    .allMatch(InventoryResponse::isInStock);
+
+            if (allProductsInStock) {
+                orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
+                return "Order Placed Successfully";
+            } else {
+                throw new IllegalArgumentException("Product is not in stock, please try again later");
+            }
+        } finally {
+            inventoryServiceLookup.flush();
         }
+
     }
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
         OrderLineItems orderLineItems = new OrderLineItems();
